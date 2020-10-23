034269a344e751d905ca4f21dff664b781d16fbb
enliple2015 <enliple2015@enliple2015ui-MacBookPro.local>
2020-10-23 15:10:16 +0900

    add Examples

---
+++ Examples/MobonSdkSample/MobonSdkSample/ViewController.swift
@@ -0,0 +1,95 @@
+//
+//  ViewController.swift
+//  MobonSdkSample
+//
+//  Created by enliple2015 on 2020/10/13.
+//
+
+import UIKit
+import MobonSDKFramework
+
+class ViewController: UIViewController,MobonBannerAdViewDelegate,MobonInterstitialAdViewDelegate {
+
+    let MEDIACODE : String = "mobon"
+    let UNITID : String = "432394"
+    var interstitialView : MobonInterstitialAdView?
+    
+    
+    @IBAction func intertstitialLoad(_ sender: Any) {
+        interstitialView?.loadAd()
+    }
+    
+    @IBAction func interstitalShow(_ sender: Any) {
+        if((interstitialView?.isViewLoaded) != nil){
+            interstitialView?.show()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    
+        let bannerView = MobonBannerAdView(frame: CGRect(x: 0, y: view.frame.size.height - 50, width: view.bounds.width, height: 50),mediaCode:MEDIACODE,sCode:UNITID)
+         bannerView.rootViewController = self
+         bannerView.delegate = self
+        bannerView.setLog = true
+         view.addSubview(bannerView)
+         bannerView.loadAd()
+        
+        
+        
+        interstitialView = MobonInterstitialAdView(mediaCode:MEDIACODE,unitId:UNITID)
+        interstitialView?.rootViewController = self
+        interstitialView?.delegate = self
+        interstitialView?.setLog = true;
+    }
+
+
+    func adViewDidReceiveAd(_ bannerAdView: MobonBannerAdView) {
+        print("adViewDidReceiveAd")
+        
+    }
+    
+    
+    func adViewFailedAd(_ bannerAdView: MobonBannerAdView, _ error: String) {
+        print("adViewFailedAd : \(error)");
+    }
+    
+    func adViewClicked(_ bannerAdView: MobonBannerAdView) {
+        print("adView Click")
+    }
+    
+    func interstitialDidReceiveAd(_ interstitialAdView: MobonInterstitialAdView) {
+        print("interstitialDidReceiveAd")
+        showPopup(controller: self, message: "interstitialDidReceiveAd", seconds: 1.0)
+    }
+    
+    
+    func interstitialFailedAd(_ interstitialAdView: MobonInterstitialAdView, _ error: String) {
+        print("interstitialFailedAd : \(error)");
+        showPopup(controller: self, message: "interstitialFailedAd : \(error)", seconds: 1.0)
+    }
+    
+    func interstitialClicked(_ interstitialAdView: MobonInterstitialAdView) {
+        print("interstitialClicked")
+    }
+    
+    func interstitialClose(_ interstitialAdView: MobonInterstitialAdView) {
+        print("interstitialClose")
+    }
+    
+    func showPopup(controller: UIViewController,message : String, seconds:Double){
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.view.backgroundColor = UIColor.black
+        alert.view.alpha = 0.9
+        alert.view.layer.cornerRadius = 15
+        
+        controller.present(alert,animated: true)
+        
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds){
+            alert.dismiss(animated: true)
+        }
+    }
+    
+}
+
---
+++ Examples/MobonSdkSample/Pods/MobonSDK/MobonSDKFramework.framework/Headers/MobonSDKFramework.h
@@ -0,0 +1,19 @@
+//
+//  MobonSDKFramework.h
+//  MobonSDKFramework
+//
+//  Created by enliple2015 on 2020/09/10.
+//  Copyright © 2020 enliple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MobonSDKFramework.
+FOUNDATION_EXPORT double MobonSDKFrameworkVersionNumber;
+
+//! Project version string for MobonSDKFramework.
+FOUNDATION_EXPORT const unsigned char MobonSDKFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MobonSDKFramework/PublicHeader.h>
+
+
